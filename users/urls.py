ea7faa08b4d25752e62f477da50036386f6e0e18
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-08-06 20:33:35 +0700

    Add user registration

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from users import views
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.UserListView.as_view(), name='index')
+    path('', views.UserListView.as_view(), name='index'),
+    path('create/', views.UserCreateView.as_view(), name='create')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
+
+from users.forms import RegisterForm
 
 
 class UserListView(ListView):
@@ -7,3 +15,11 @@ class UserListView(ListView):
     paginate_by = 20
     template_name = 'users/index.html'
     context_object_name = 'users'
+
+
+class UserCreateView(SuccessMessageMixin, CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = 'users/create.html'
+    success_url = reverse_lazy('users:index')
+    success_message = _('User successfully registered')
