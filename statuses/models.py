6fa6df54ec4c5d59bc56be0234b2b01f4795dcae
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-08-11 20:20:42 +0700

    Update Russian localization

--- statuses/models.py
+++ statuses/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation time'))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Last update time'))
 
     class Meta:
         verbose_name = _('status')
