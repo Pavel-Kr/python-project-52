dc6af74de151231ac8d4d7eb213e65072e1255a8
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-08-20 20:49:23 +0700

    Add details page for task

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,6 @@ from tasks import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='index'),
+    path('<int:pk>/', views.TaskDetailView.as_view(), name='details'),
     path('create/', views.TaskCreateView.as_view(), name='create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
@@ -14,6 +14,21 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self) -> QuerySet[Any]:
         return Task.objects.select_related(
+            'author'
+        ).select_related(
+            'performer'
+        ).select_related(
+            'status'
+        )
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'tasks/show.html'
+    model = Task
+    context_object_name = 'task'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().select_related(
             'author'
         ).select_related(
             'performer'
